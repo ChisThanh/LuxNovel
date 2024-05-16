908f25d47d9f1858a1d4c3757b116e9e7019c3b8
Lattecrone <helloagaindumbel@gmail.com>
2024-05-16 19:39:26 +0700

    Add activity_login
    Giao diện của Activity Login

---
+++ app/src/main/java/project/luxnovel/Activity/ActivityRegister.java
@@ -0,0 +1,17 @@
+package project.luxnovel.Activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import project.luxnovel.R;
+
+public class ActivityRegister extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
